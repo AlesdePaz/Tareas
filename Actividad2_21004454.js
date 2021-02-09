9ba50884d38435184ddae587336fc56efab594c4
Ulises de Paz <alesdepaz16@gmail.com>
2021-02-08 21:13:40 -0600

    First commit

---
+++ Actividad2_21004454.js
@@ -0,0 +1,13 @@
+var mes = 10
+if (mes == 12 || (mes <=2 && mes >0) ) {
+    alert("Es invierno")
+}else if(mes >= 3 && mes <= 5 ){
+  alert("Es primavera")
+} else if(mes >= 6 && mes <= 8 ){
+  alert("Es verano")
+}else if(mes >= 9 && mes <= 11 ){
+  alert("Es otoño")
+}else { 
+alert("este mes no existe")
+}
+alert("Carné: 21004454, Ulíses Alescio de Paz Rivera")
